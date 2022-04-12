d6d1927f5777bf8ea60e192deb3641a94406aa9b
Jakub Theimer <jakub@dy.st>
2022-04-12 23:34:02 +0200

    add multiselect translation file

---
+++ resources/lang/en/multiselect.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'label' => 'Multiselect',
+    'missing_labels' => 'You must ensure each row as a label',
+    'value_placeholder' => 'Leave blank to use label',
+    'label_heading' => 'Label',
+    'value_heading' => 'Value',
+    'add_row_btn' => 'Add Row',
+    'empty_selection' => 'Select at least one value',
+];
