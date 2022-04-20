8015866f6eb3104cea1d681fcbb9b90d81b24005
Jakub Theimer <jakub@dy.st>
2022-04-20 22:18:16 +0200

    compile multiselect.js instead of app.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require("laravel-mix");
 
-mix.ts("resources/ts/index.ts", "dist/app.js").setPublicPath("dist");
+mix.ts("resources/ts/index.ts", "dist/multiselect.js").setPublicPath("dist");
 
 if (mix.inProduction()) {
-  mix.version();
+    mix.version();
 }
 
 mix.disableSuccessNotifications();
